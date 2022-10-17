9151c6cffb83b83614cbfb645459a61bf1cb9963
M Aan Dafa Setiawan <kamikazesetan@gmail.com>
2022-10-17 17:11:56 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <nav class="sticky top-0 bg-gray-50 w-full border-b-4 border-indigo-500 font-semibold">
+        <div class="flex container mx-auto py-4">
+            <img src="https://akcdn.detik.net.id/community/media/visual/2020/09/17/logo-detiknews.png?d=1" alt="" class="w-32 mr-4">
+            <ul class="flex space-x-8 text-slate-700 ">
+                <li>
+                    <a href="#" class="hover:text-sky-500">Menu 1</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:text-sky-500">Menu 2</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:text-sky-500">Menu 3</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+
+    <header>
+        <img src="https://source.unsplash.com/random/featured/?paper" class="bg-cover w-screen h-screen" alt="">
+    </header>
+
+    <section class="bg-gray-200">
+        <div class="container mx-auto py-4">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, non. Nobis, facere voluptas? Magni sunt ea suscipit quasi. Recusandae architecto doloremque hic quos reiciendis a corporis rem, dicta ut aperiam quis nesciunt officia aliquam eligendi unde ipsum provident saepe voluptatem exercitationem consequuntur, numquam perferendis? Eligendi quos fugit beatae dolore aut saepe error eaque est aperiam aspernatur necessitatibus magni officiis dolores perspiciatis numquam quidem omnis animi qui dolor illo architecto, reprehenderit voluptate! Ab laudantium aperiam neque nisi? Consequatur, deleniti, laboriosam culpa numquam tenetur sit, voluptate perspiciatis vitae doloribus non aliquam! Deserunt provident consectetur sequi ducimus distinctio minima temporibus blanditiis ipsam ipsum?</br>
+
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus fuga illum dicta amet. Temporibus omnis officia quaerat eaque totam nisi quos ad illum nostrum atque aliquid, illo fuga nobis. Placeat deleniti similique magnam! Perspiciatis id nam repellendus at voluptates, ab asperiores minima voluptatibus similique natus neque ducimus vitae, laudantium, tempora eum harum? Iste soluta dolor quibusdam saepe nostrum hic. Quas necessitatibus eligendi voluptatem placeat vero veritatis exercitationem, cumque tempore, voluptates nobis vel labore similique iure impedit! Molestias tempore, quos recusandae eligendi culpa temporibus laudantium? Blanditiis obcaecati, beatae nisi consequatur, animi ipsa neque odit suscipit cumque placeat eius impedit rem cum.
+            </p>
+        </div>
+    </section>
+
+    <section class="bg-sky-200">
+        <div class="container mx-auto py-4">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, non. Nobis, facere voluptas? Magni sunt ea suscipit quasi. Recusandae architecto doloremque hic quos reiciendis a corporis rem, dicta ut aperiam quis nesciunt officia aliquam eligendi unde ipsum provident saepe voluptatem exercitationem consequuntur, numquam perferendis? Eligendi quos fugit beatae dolore aut saepe error eaque est aperiam aspernatur necessitatibus magni officiis dolores perspiciatis numquam quidem omnis animi qui dolor illo architecto, reprehenderit voluptate! Ab laudantium aperiam neque nisi? Consequatur, deleniti, laboriosam culpa numquam tenetur sit, voluptate perspiciatis vitae doloribus non aliquam! Deserunt provident consectetur sequi ducimus distinctio minima temporibus blanditiis ipsam ipsum?</br>
+
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus fuga illum dicta amet. Temporibus omnis officia quaerat eaque totam nisi quos ad illum nostrum atque aliquid, illo fuga nobis. Placeat deleniti similique magnam! Perspiciatis id nam repellendus at voluptates, ab asperiores minima voluptatibus similique natus neque ducimus vitae, laudantium, tempora eum harum? Iste soluta dolor quibusdam saepe nostrum hic. Quas necessitatibus eligendi voluptatem placeat vero veritatis exercitationem, cumque tempore, voluptates nobis vel labore similique iure impedit! Molestias tempore, quos recusandae eligendi culpa temporibus laudantium? Blanditiis obcaecati, beatae nisi consequatur, animi ipsa neque odit suscipit cumque placeat eius impedit rem cum.
+            </p>
+        </div>
+    </section>
+
+
+</body>
+
+</html>
